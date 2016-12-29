51c1afe99db3a333847430de202b3891e8724b1c
ListenerGao <hnthgys@gmail.com>
2016-12-29 11:47:40 +0800

    Initial commit

---
+++ app/src/main/java/com/listenergao/customprogressbar/MainActivity.java
@@ -0,0 +1,38 @@
+package com.listenergao.customprogressbar;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+
+import com.listenergao.customprogressbar_library.HorizontalProgressBarWithProgress;
+import com.listenergao.customprogressbar_library.RoundProgressBarWithProgress;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int MSG_UPDATE = 0x110;
+    private HorizontalProgressBarWithProgress mHProgress;
+    private RoundProgressBarWithProgress mRoundProgress;
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            int progress = mHProgress.getProgress();
+            mHProgress.setProgress(++progress);
+            mRoundProgress.setProgress(++progress);
+            if (progress > 100) {
+                mHandler.removeMessages(MSG_UPDATE);
+            }
+            mHandler.sendEmptyMessageDelayed(MSG_UPDATE,100);
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mHProgress = (HorizontalProgressBarWithProgress) findViewById(R.id.progress);
+        mRoundProgress = (RoundProgressBarWithProgress) findViewById(R.id.round_progress);
+        mHandler.sendEmptyMessage(MSG_UPDATE);
+    }
+}
